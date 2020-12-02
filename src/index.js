6aa3ffb98c2c1e4e1490e97d93c720f9434b185c
inezabonte <inezabonte@gmail.com>
2020-12-02 15:16:03 +0200

    I set up redux and added a counter

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
-import React, { Component } from 'react'
+import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
-// import Hi from './App'
+import { Provider } from 'react-redux'
+import store from './store'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>, 
+  document.getElementById('root')
+);
