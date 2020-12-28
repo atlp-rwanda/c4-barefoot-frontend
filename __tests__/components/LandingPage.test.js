5a774b8aee68a2e49798d90170914076c5c674ff
inezabonte <inezabonte@gmail.com>
2020-12-29 01:04:32 +0200

    testing component

--- __tests__/components/LandingPage.test.js
+++ __tests__/components/LandingPage.test.js
@@ -5,9 +5,15 @@ import { shallow , mount} from "enzyme";
 import configureStore from 'redux-mock-store';
 import { Provider } from 'react-redux'
 import toJson from 'enzyme-to-json';
-const mockStore = configureStore([]);
+import thunk from 'redux-thunk';
+import { Container } from '@material-ui/core'
+import { Skeleton } from '@material-ui/lab'
 
-const store = mockStore({
+const middlewares = [thunk]
+const mockStore = configureStore(middlewares);
+let store;
+
+store = mockStore({
   fetchLocations: locationState,
   fetchAccommodations:  accommodationState,
 })
@@ -17,26 +23,39 @@ const props = {
   getAccommodations: jest.fn()
 }
 
-const landingPageComponent = shallow(
-  <Provider store = {store}>
-    <LandingPage/>
-  </Provider>
-)
-
 describe('Landing Page test', () => {
   
   it('Renders without crashing', () => {
-   const tree = toJson(landingPageComponent)
-   expect(tree).toMatchSnapshot();
+    const landingPageComponent = shallow(
+      <Provider store = {store}>
+        <LandingPage/>
+      </Provider>
+    )
+    const tree = toJson(landingPageComponent)
+    expect(tree).toMatchSnapshot();
     
   })
 
-  // it("renders correctly", () => {
-  //   const wrapper = mount(
-  //   <Provider store = {store}>
-  //     <LandingPage {...props} />
-  //   </Provider>);
-  //   expect(wrapper.state("error")).toEqual(null);
-  // });
+  it("renders correctly", () => {
+    const wrapper = mount(
+    <Provider store = {store}>
+      <LandingPage {...props} />
+    </Provider>);
+    expect(wrapper.find(Container).length).toBe(2);
+  });
+
+  it("Shows skeletons when there is no data", () => {
+    store = mockStore({
+      fetchLocations: {pending: true},
+      fetchAccommodations:  {pending: true},
+    })
+
+    const wrapper = mount(
+      <Provider store = {store}>
+        <LandingPage {...props} />
+      </Provider>);
+
+    expect(wrapper.find(Skeleton).length).toBe(13)
+  })
 })
 
