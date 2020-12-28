81acc1182039dd44141b8466711cf0fc8bb17ca2
mugiranezaj <jackswalter7@gmail.com>
2020-12-28 17:35:03 +0200

    wrapping tests into act()

--- __tests__/components/signup.test.js
+++ __tests__/components/signup.test.js
@@ -8,6 +8,7 @@ import { Confirm } from "../../src/components/signup/thirdStep";
 import verifyAccount from "../../src/components/signup/verifyAccount";
 import SocialButtons from '../../src/components/signup/socialButton'
 import SideDiv from "../../src/components/signup/sideDiv";
+import { act } from "react-test-renderer";
 
 describe('Signup', () => {
     let formData = new FormData()
@@ -44,18 +45,23 @@ describe('Signup', () => {
         
     })
     describe('Functionalities', () => {
-        it('should call onChange prop with input value', () => {
-            const onSearchMock = jest.fn();
-            const component = mount(<SecondStep onChange={onSearchMock} {...props} value="custom value" />);
-            component.find('#file').simulate('change');
-            // expect(onSearchMock).toBeCalledWith('image.jpg');
+        it('should call onChange prop with input value', async () => {
+           await act( async () => {
+                const onSearchMock = jest.fn();
+                const component = mount(<SecondStep onChange={onSearchMock} {...props} value="custom value" />);
+                component.find('#file').simulate('change');
+                // expect(onSearchMock).toBeCalledWith('image.jpg');
+           })
+            
         });
-        it('should call onClick to submit form', () => {
-            const signupRequest = jest.fn();
-            const handleClose = jest.fn();
-            const component = mount(<Confirm onClick={signupRequest} handleClose={handleClose} {...props} value="custom value" />);
-            component.find('[btn="submitBtn"]').first().simulate('click');
-            // expect(onSearchMock).toBeCalledWith('image.jpg');
+        it('should call onClick to submit form', async () => {
+            await act( async () => {
+                const signupRequest = jest.fn();
+                const handleClose = jest.fn();
+                const component = mount(<Confirm onClick={signupRequest} handleClose={handleClose} {...props} value="custom value" />);
+                component.find('[btn="submitBtn"]').first().simulate('click');
+                // expect(onSearchMock).toBeCalledWith('image.jpg');
+            })
         });
     })
 
