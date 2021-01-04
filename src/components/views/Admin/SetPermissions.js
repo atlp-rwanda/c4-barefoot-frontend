736dfa1a068119adb481ba1954d67a35dd2e72ef
inezabonte <inezabonte@gmail.com>
2021-01-04 10:12:52 +0200

    permissions styles

--- src/components/views/Admin/SetPermissions.js
+++ src/components/views/Admin/SetPermissions.js
@@ -1,13 +1,23 @@
 import React from 'react'
 import {Typography, makeStyles, Box, Divider, Container} from '@material-ui/core'
 
+const useStyles = makeStyles((theme) => ({
+ root: {
+   display: 'flex',
+   flexDirection: 'column',
+   alignContent: 'center',
+   justifyContent: 'center'
+ }
+}))
+
 function SetPermissions(){
+  const classes = useStyles()
   return(
-    <Container>
+    <Container className={classes.root}>
       <Box>
         <Typography variant='h5' >Allow users to perform their operations</Typography>
         <Divider/>
-    </Box>
+      </Box>
     </Container>
     
   )
