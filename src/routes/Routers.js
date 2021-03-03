25c0fd5154dfae601e470f6bc089be8b6965f10f
edgar929 <shumbushoedgar@gmail.com>
2021-03-03 15:45:30 +0200

    fixing conflicts

--- src/routes/Routers.js
+++ src/routes/Routers.js
@@ -11,96 +11,6 @@ import RouteWithLayout from '../components/RouteWithLayout';
 import Profile from '../components/views/Profile';
 import { DefaultLayout, AuthorizedUserLayout } from '../components/layouts';
 import Logout from '../components/views/Logout';
-<<<<<<< HEAD
-import UserProfile from '../components/views/userProfile';
-import ChangePassword from '../components/views/changePassword';
-import LoggedInUserLayout from '../components/layouts/LoggedInUser/LoggedInUser';
-
-const Routes = () => {
-  return (
-    <Switch>
-      <Redirect
-        exact
-        from="/"
-        to="/welcome"
-      />
-      <RouteWithLayout
-        component={Landing}
-        exact
-        layout={DefaultLayout}
-        path="/welcome"
-      />
-      <RouteWithLayout
-        component={Login}
-        exact
-        layout={DefaultLayout}
-        path="/login"
-      />
-      <RouteWithLayout
-        component={UserProfile}
-        exact
-        layout={LoggedInUserLayout}
-        path="/profile"
-      />
-      <RouteWithLayout
-        component={ChangePassword}
-        exact
-        layout={LoggedInUserLayout}
-        path="/changePassword"
-      />
-      <RouteWithLayout
-        component={signup}
-        exact
-        layout={DefaultLayout}
-        path="/signup"
-      />
-      <RouteWithLayout
-        component={ResetPasswordEmailForm}
-        exact
-        layout={DefaultLayout}
-        path="/forgetpassword"
-      />
-      <RouteWithLayout
-        component={NewPassword}
-        exact
-        layout={DefaultLayout}
-        path="/user/reset-password"
-      />
-      <RouteWithLayout
-        component={Logout}
-        exact
-        layout={AuthorizedUserLayout}
-        path="/logout"
-      />
-      <RouteWithLayout
-        component={PageNotFound}
-        exact
-        layout={DefaultLayout}
-        path="/PageNotFound"
-      />
-      <RouteWithLayout
-        path="/signup"
-        component={signup}
-        layout={DefaultLayout}
-      />
-      <RouteWithLayout
-        path="/user/verification/:token"
-        component={verifyAccount}
-        layout={DefaultLayout}
-      />
-      <RouteWithLayout
-        path="/user/verification/"
-        component={verifyAccount}
-        layout={DefaultLayout}
-      />
-
-      <Redirect to="/PageNotFound" />
-    </Switch>
-  );
-};
-
-export default Routes;
-=======
 import ProtectedRoute from './protected.route'
 
 const Routes = () => {
@@ -181,4 +91,3 @@ const Routes = () => {
   };
   
   export default Routes;
->>>>>>> develop
