e6cce582382d2970b4b2c6f011d935e5e2e95d61
MUNGERI Frank <mungerifrank@gmail.com>
2021-03-03 08:53:59 +0200

    ft(manager-sidenav): use the side navigation as Noel

--- src/routes/Routers.js
+++ src/routes/Routers.js
@@ -8,14 +8,7 @@ import Landing from '../components/views/LandingPage'
 import PageNotFound from '../components/views/PageNotFound'
 import Unauthorized from '../components/views/Unauthorized'
 import Profile from '../components/views/Profile';
-import adminHome from '../components/views/Admin/Home';
-import CreateRoles from '../components/views/Admin/CreateRoles';
-import SetPermissions from '../components/views/Admin/SetPermissions'
-import ListOfRoles from '../components/views/Admin/ListOfRoles'
-import ListUsers from '../components/views/Admin/ListUsers'
-import { Switch, Redirect } from 'react-router-dom';
-import RouteWithLayout from '../components/RouteWithLayout';
-import {  DefaultLayout, AuthorizedUserLayout, AdminLayout, ErrorLayout, ManagerLayout } from '../components/layouts';
+import { DefaultLayout, AuthorizedUserLayout, ManagerLayout } from '../components/layouts';
 import Logout from '../components/views/Logout';
 // import ManagerDashboard from '../components/sideBarDrawer/ManagerDashboard';
 import ManagerTravelDashboard from '../components/manageTravel/manageTravelDashboard'
@@ -182,7 +175,7 @@ const Routes = () => {
             path="/assign-users-to-managers/"
             exact
             component= { AssignUsersToManagers }
-           layout={AuthorizedUserLayout}
+           layout={ManagerLayout}
         />
         <Redirect to="/PageNotFound" />
       </Switch>
