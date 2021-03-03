c250c8481da468d1e220dafc988d9f2262a1e776
Blaiseniyo <blaiseniyonkuru12@gmail.com>
2021-03-03 15:20:09 +0200

    fixing named import

--- src/components/layouts/adminLayout/Default.js
+++ src/components/layouts/adminLayout/Default.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/styles';
-import AdminNavBar from '../../AdminNavbar'
+import AdminNavBar from '../../adminNavbar'
 import Footer from '../../Footer'
 
 
