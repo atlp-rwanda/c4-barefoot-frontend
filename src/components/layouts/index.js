5d658955ff72a1633b845dff02ef1c9428646f2e
MUNGERI Frank <mungerifrank@gmail.com>
2021-03-03 08:53:59 +0200

    ft(manager-sidenav): use the side navigation as Noel

--- src/components/layouts/index.js
+++ src/components/layouts/index.js
@@ -1,6 +1,4 @@
 export { default as DefaultLayout} from './Default';
 export { default as AdminLayout } from './adminLayout'
 export { default as AuthorizedUserLayout } from './AuthorizedUser';
-export {default as ManagerLayout} from './managerLayout'
-
-export{ default as ErrorLayout } from './ErrorLayout'
+export { default as ManagerLayout } from './ManagerLayout';
