799e2371b009f872fcf43f1de48beaa0901d6692
Blaiseniyo <62561244+Blaiseniyo@users.noreply.github.com>
2021-03-11 12:22:29 +0200

    Ft create roles permissions (#6)
    * created layout for admin
    
    * added data on home screen
    
    * created roles page
    
    * permissions styles
    
    * added count to redux
    
    * made pending true by default
    
    * added data on the home page
    
    * adding roles to database
    
    * created columns for set permissions
    
    * roles and users pages
    
    * users page
    
    * setting permissions
    
    * changed the endpoints
    
    * manager's actions and reducers
    
    * merging files from remote
    
    * refactoring the create roles page
    
    * adding the setpermissions and create roles component
    
    * fixing named import
    
    * fixing named import
    
    * fixing named import
    
    * adding title on list of roles component,succes message on update role
    
    * fixing tests that are failling
    
    * adding some testes and fixing some test
    
    * rebasing
    
    Co-authored-by: inezabonte <inezabonte@gmail.com>

--- src/redux/actions/fetchLocationsAction.js
+++ src/redux/actions/fetchLocationsAction.js
@@ -1,19 +1,14 @@
-import axios from 'axios'
-
-export const FETCH_LOCATIONS_PENDING = 'FETCH_LOCATIONS_PENDING'
+import {API} from './AxiosAPI';
 export const  FETCH_LOCATIONS_SUCCESS = 'FETCH_LOCATIONS_SUCCESS'
 export const FETCH_LOCATIONS_ERROR = 'FETCH_LOCATIONS_ERROR'
 
 
 export const getLocations = () => dispatch => {
-  dispatch({
-    type: FETCH_LOCATIONS_PENDING
-  })
-  return axios.get(`${process.env.REACT_APP_BACKEND_LINK}/locations`)
+  return API.get(`/locations`)
     .then(res => {
       dispatch({
         type: FETCH_LOCATIONS_SUCCESS,
-        payload: res.data.locations.rows
+        payload: res.data.locations
       })
       }
     )
