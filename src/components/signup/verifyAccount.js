f6a4184a9a08862aa905ddce1ac10a3546ae62c1
Blaiseniyo <62561244+Blaiseniyo@users.noreply.github.com>
2021-03-11 12:22:29 +0200

    Ft create roles permissions (#6)
    * created layout for admin
    
    * added data on home screen
    
    * created roles page
    
    * permissions styles
    
    * added count to redux
    
    * made pending true by default
    
    * added data on the home page
    
    * adding roles to database
    
    * created columns for set permissions
    
    * roles and users pages
    
    * users page
    
    * setting permissions
    
    * changed the endpoints
    
    * manager's actions and reducers
    
    * merging files from remote
    
    * refactoring the create roles page
    
    * adding the setpermissions and create roles component
    
    * fixing named import
    
    * fixing named import
    
    * fixing named import
    
    * adding title on list of roles component,succes message on update role
    
    * fixing tests that are failling
    
    * adding some testes and fixing some test
    
    * rebasing
    
    Co-authored-by: inezabonte <inezabonte@gmail.com>

--- src/components/signup/verifyAccount.js
+++ src/components/signup/verifyAccount.js
@@ -2,6 +2,7 @@ import { Box, Button, makeStyles } from '@material-ui/core'
 import Devider from '@material-ui/core/Divider'
 import { CheckCircle } from '@material-ui/icons'
 import Axios from 'axios'
+import {API} from '../../redux/actions/AxiosAPI';
 import React, { useState } from 'react'
 import queryString from 'query-string';
 import { Skeleton } from '@material-ui/lab'
@@ -40,7 +41,7 @@ const useStyle = makeStyles(theme => ({
     }
 }))
 const verify = (token, verified, setVerified ,setError) => {
-    Axios.patch(`https://barefoot-nomad-app-v1.herokuapp.com/api/v1/user/verification?token=` + token)
+    API.patch('user/verification?token=' + token)
     .then(res => {
       setVerified(true)
     })
