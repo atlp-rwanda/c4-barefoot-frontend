bf6810a9a8a7e49bd3cc43ee807ab9aa2c31fa38
MUNGERI Frank <mungerifrank@gmail.com>
2021-03-17 18:04:22 +0200

    ft:assign users to managers

--- src/components/assignUsersToManagers/index.js
+++ src/components/assignUsersToManagers/index.js
@@ -1,28 +1,47 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Container } from '@material-ui/core';
 import { connect, useDispatch } from 'react-redux';
 import UsersList from './UsersList';
 import { getManagersList, getVerifiedUsers } from '../../redux/actions/assignUserActions';
+import { usersListPage } from '../../redux/actions/assignUserActions';
 import FailureDialog from './FailureDialog';
 import SuccessDialog from './SuccessDialog';
 
 const AssignUsersToManagers = (props) => {
   const { addAssignActionToQueue, fetchVerifiedUsers, fetchAllManagers } = props;
-  const successDialog = true, failureDialog = !successDialog;
+  let dialog = false;
+  if(addAssignActionToQueue instanceof Promise) {
+    const something = addAssignActionToQueue.then(res => res);
+    const succeededDialog = (
+      addAssignActionToQueue.hasOwnProperty('success')
+      && addAssignActionToQueue.success.length != 0 
+      && addAssignActionToQueue.errors.length == 0
+    );
+    console.log({something});
+  }
   const pending = (fetchAllManagers.pending && fetchVerifiedUsers.pending)
                   && !fetchAllManagers.loaded;
-  console.log({fetchVerifiedUsers,fetchAllManagers, addAssignActionToQueue});
+  console.log({fetchVerifiedUsers, fetchAllManagers, addAssignActionToQueue});
   const dispatch = useDispatch();
+  const [page, handlePage] = useState(1);
   useEffect(() => {
-    props.getVerifiedUsers(dispatch);
+    props.getVerifiedUsers(page, dispatch);
     props.getManagersList(dispatch);
   }, []);
-
+  
+  const handlePageChange = (evt, page) => {
+    usersListPage(page)(dispatch);
+  }
+  console.log({addAssignActionToQueue}, addAssignActionToQueue.hasOwnProperty('success'));
   return (
     <Container style={{display: 'flex', flexDirection: 'row', justify: 'flex-start', padding: 'unset'}}>
-      <UsersList loading={pending} />
-        <SuccessDialog open={successDialog} />
-        <FailureDialog open={failureDialog}/>
+      <UsersList loading={pending} page={page} handlePageChange={handlePageChange} />
+      {
+        dialog
+        ? ( succeededDialog ? <SuccessDialog open={true} /> : <FailureDialog open={false} /> )
+        : <></>
+        // addAssignActionToQueue.hasOwnProperty('success') ? <SuccessDialog />
+      }
     </Container>
   );
 }
--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -23,6 +23,8 @@ import createAccReducer from './createAccReducer';
 import createLocReducer from './createLocReducer';
 import { fetchAllManagers, fetchVerifiedUsers } from './assignUserReducers';
 import addAssignActionToQueue from './addAssignActionToQueue';
+import fetchVerifiedUsersPage from './fetchVerifiedUsersPage';
+import assigningMessage from './assigningMessage';
 
 const reducers = combineReducers({
   logout: logoutReducer,
@@ -51,7 +53,9 @@ const reducers = combineReducers({
   fetchVerifiedUsers: fetchVerifiedUsers,
   fetchAllManagers: fetchAllManagers,
   addAssignActionToQueue: addAssignActionToQueue,
-  managerReducer  
+  fetchVerifiedUsersPage: fetchVerifiedUsersPage,
+  assigningMessage: assigningMessage,
+  managerReducer
 });
 
 export default reducers
