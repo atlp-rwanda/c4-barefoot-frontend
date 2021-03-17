e21db2d898eae0d9b6bbd6fa5dc8725b9e5ae04c
songa1 <songachillethe1her@gmail.com>
2021-03-17 10:12:49 +0200

    I can now upload photos in messages

--- src/components/Chat/Lists/NewMessage.js
+++ src/components/Chat/Lists/NewMessage.js
@@ -1,9 +1,12 @@
 import React from 'react';
-import { Paper, Toolbar, Input, InputAdornment, IconButton, FormHelperText } from '@material-ui/core';
+import axios from 'axios';
+import { Paper, Toolbar, Input, InputAdornment, IconButton, FormHelperText, CircularProgress } from '@material-ui/core';
 import { connect } from 'react-redux';
 import { newMessageAction, supportResponds } from '../../../redux/actions/ChatAction'
 import SendIcon from '@material-ui/icons/Send';
 import AttachFileIcon from '@material-ui/icons/AttachFile';
+
+
 class NewMessage extends React.Component {
     constructor(props) {
         super(props);
@@ -11,12 +14,13 @@ class NewMessage extends React.Component {
             message: '',
             vmessage: '',
             image: '',
-            feedbackText: ''
+            feedbackText: '',
+            loading: false,
+            vimage: ''
         }
         this.handleChange = this.handleChange.bind(this)
         this.handleClick = this.handleClick.bind(this)
         this.vhandleClick = this.vhandleClick.bind(this)
-        this.handleUpload = this.handleUpload.bind(this)
     }
 
     handleChange = (e) => {
@@ -27,33 +31,50 @@ class NewMessage extends React.Component {
 
     handleClick = () => {
         const receiverId = localStorage.getItem('userId')
-        if(this.state.message === ''){
-            this.setState({
-                feedbackText: "Message can't be blank!"
-            })
-        }else{
+        if(this.state.message != ''){
             const messageData = {
                 receiver: receiverId,
                 message: this.state.message,
                 type: 'plain-text'
             }
             this.props.newMessageAction(messageData);
+            
+        }else if(this.state.image != ''){
+            const messageData = {
+                receiver: receiverId,
+                message: this.state.image,
+                type: 'image'
+            }
+            this.props.newMessageAction(messageData);
+        }else{
+            this.setState({
+                feedbackText: "Message can't be blank!"
+            })
         } 
-    }
-    handleUpload = () => {
-        
     }
 
     vhandleClick = () => {
         const receiverId = localStorage.getItem('userId')
-        console.log(receiverId)
-        const messageData = {
-            visitor: receiverId,
-            message: this.state.vmessage,
-            type: 'plain-text'
-        }
-        console.log(messageData)
-        this.props.supportResponds(messageData);
+        if(this.state.vmessage != ''){
+            const messageData = {
+                receiver: receiverId,
+                message: this.state.vmessage,
+                type: 'plain-text'
+            }
+            this.props.newMessageAction(messageData);
+            
+        }else if(this.state.vimage != ''){
+            const messageData = {
+                receiver: receiverId,
+                message: this.state.vimage,
+                type: 'image'
+            }
+            this.props.newMessageAction(messageData);
+        }else{
+            this.setState({
+                feedbackText: "Message can't be blank!"
+            })
+        } 
     }
 
     render() {
@@ -70,10 +91,33 @@ class NewMessage extends React.Component {
                             name='message'
                             endAdornment={
                                 <InputAdornment position="end">
-                                    <IconButton onClick={this.handleUpload}>
                                     
-                                        
-                                    </IconButton><IconButton onClick={this.handleClick}><a href='' style={{textDecoration: 'none', color: 'inherit'}}><SendIcon/></a></IconButton>
+                                    <input 
+                                        id="file" 
+                                        name="message_img" 
+                                        type="file"
+                                        hidden={true}
+                                        onChange={(e) => {
+                                            const profile_picture = e.target.files[0]
+                                            const formData = new FormData()
+                                            formData.append('upload_preset', process.env.UPLOAD_PRESET)
+                                            formData.append('file', profile_picture)
+                                            this.setState({loading: true})            
+                                            axios.post(process.env.IMAGE_UPLOAD_LINK, formData)
+                                            .then(res => {
+                                                this.setState({loading: false}) 
+                                                this.setState({image: res.data.secure_url})
+                                            })
+                                            .catch(err => {
+                                                this.setState({
+                                                    loading: false,
+                                                    feedbackText: err.message
+                                                })
+                                            })
+                                        }}
+                                    />
+                                    <label htmlFor="file">{this.state.loading === false ? <AttachFileIcon/>: <CircularProgress color="primary" />}</label>
+                                    <IconButton onClick={this.handleClick}><a href='' style={{textDecoration: 'none', color: 'inherit'}}><SendIcon/></a></IconButton>
                                 </InputAdornment>
                             }
                         />
@@ -83,19 +127,46 @@ class NewMessage extends React.Component {
                 <Paper position="fixed" style={{bottom:0, top: 'auto', left: 0, right: 0, borderRadius: 10, marginTop: '7px', padding: '10px', boxSizing: 'border-box'}}>
                     <Toolbar position='fixed' color="primary">
                         <FormControl>
-                        <Input border={0} 
-                            variant="outlined" 
-                            style={{width: '100%', borderRadius: 50}}
-                            value={this.state.vmessage}
-                            onChange={this.handleChange}
-                            name='vmessage'
-                            endAdornment={
-                                <InputAdornment position="end">
-                                    <IconButton><AttachFileIcon/></IconButton><IconButton onClick={this.vhandleClick}><a href='' style={{textDecoration: 'none', color: 'inherit'}}><SendIcon/></a></IconButton>
-                                </InputAdornment>
-                            }
-                        />
-                        <FormHelperText>{this.state.feedbackText}</FormHelperText>
+                            <Input border={0} 
+                                variant="outlined" 
+                                style={{width: '100%', borderRadius: 50}}
+                                value={this.state.vmessage}
+                                onChange={this.handleChange}
+                                name='vmessage'
+                                endAdornment={
+                                    <InputAdornment position="end">
+                                        <input 
+                                        id="file" 
+                                        name="message_img" 
+                                        type="file"
+                                        hidden={true}
+                                        onChange={(e) => {
+                                            const profile_picture = e.target.files[0]
+                                            const formData = new FormData()
+                                            formData.append('upload_preset', process.env.UPLOAD_PRESET)
+                                            formData.append('file', profile_picture)
+                                            this.setState({loading: true})            
+                                            axios.post(process.env.IMAGE_UPLOAD_LINK, formData)
+                                            .then(res => {
+                                                this.setState({
+                                                    loading: false,
+                                                    vimage: res.data.secure_url
+                                                })
+                                            })
+                                            .catch(err => {
+                                                this.setState({
+                                                    loading: false,
+                                                    feedbackText: err.message
+                                                })
+                                            })
+                                        }}
+                                    />
+                                    <label htmlFor="file">{this.state.loading === false ? <AttachFileIcon/>: <CircularProgress color="primary" />}</label>
+                                        <IconButton onClick={this.vhandleClick}><a href='' style={{textDecoration: 'none', color: 'inherit'}}><SendIcon/></a></IconButton>
+                                    </InputAdornment>
+                                }
+                            />
+                            <FormHelperText>{this.state.feedbackText}</FormHelperText>
                         </FormControl>
                     </Toolbar>
                 </Paper>}
