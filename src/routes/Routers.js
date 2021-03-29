317cd0752a529e9eff027acfc020a25c10a59746
CYIMANA Faisal <cyimanafaisal@gmail.com>
2021-03-29 11:35:56 +0200

    Resolved the comflitcs in Routers.js

--- src/routes/Routers.js
+++ src/routes/Routers.js
@@ -2,29 +2,16 @@ import ResetPasswordEmailForm from '../components/resetPassword/ResetPasswordEma
 import NewPassword from '../components/resetPassword/NewPassword'
 
 import React from 'react';
-import Login from '../components/views/Login';
-import signup from '../components/views/Signup';
-import verifyAccount from '../components/signup/verifyAccount';
-import Landing from '../components/views/LandingPage';
+import Login from '../components/views/Login'
+import signup from '../components/views/Signup'
+import verifyAccount from '../components/signup/verifyAccount'
+import Landing from '../components/views/LandingPage'
 import PageNotFound from '../components/views/PageNotFound'
 import { Switch, Redirect } from 'react-router-dom';
 import RouteWithLayout from '../components/RouteWithLayout';
 import Profile from '../components/views/Profile';
 import { DefaultLayout, AuthorizedUserLayout } from '../components/layouts';
 import Logout from '../components/views/Logout';
-<<<<<<< HEAD
-<<<<<<< HEAD
-// import ManagerDashboard from '../components/sideBarDrawer/ManagerDashboard';
-import ManagerTravelDashboard from '../components/manageTravel/manageTravelDashboard'
-import ApprovedReports from '../components/manageTravel/ApprovedReports';
-import RejectedAndCanceled from '../components/manageTravel/RejectedAndCanceledReports';
-import Done from '../components/manageTravel/Done';
-import ProtectedRoute from './protected.route'
-import userProfile from '../components/views/userProfile';
-
-=======
-=======
->>>>>>> After rebasing
 import CreateTravelRequest from '../components/views/user/CreateTravelRequest';
 import RequesterDefault from '../components/RequesterLayoutRoute/Default';
 import ViewTravelRequest from '../components/views/user/ViewTravelRequest';
@@ -32,63 +19,6 @@ import RequesterRoute from '../components/RequesterLayoutRoute';
 
 
 const Routes = () => {
-<<<<<<< HEAD
-    return (
-      <Switch>
-        <Redirect
-          exact
-          from="/"
-          to="/welcome"
-        />
-        <RouteWithLayout
-          component={Landing}
-          exact
-          layout={DefaultLayout}
-          path="/welcome"
-        />
-        <RouteWithLayout
-          component={Login}
-          exact
-          layout={DefaultLayout}
-          path="/login"
-        />
-        <ProtectedRoute
-          component={Profile}
-          exact
-          layout={AuthorizedUserLayout}
-          path="/profile"
-        />
-         <ProtectedRoute
-          component={userProfile}
-          exact
-          layout={AuthorizedUserLayout}
-          path="/userprofile"
-        />
-        {/* <RouteWithLayout
-          component={ManagerDashboard}
-          exact
-          layout={ManagerLayout}
-          path="/managerDashboard"
-        /> */}
-        <RouteWithLayout
-          component={ManagerTravelDashboard}
-          exact
-          layout={ManagerLayout}
-          path="/managerTravel"
-        />
-        <RouteWithLayout
-          component={ApprovedReports}
-          exact
-          layout={ManagerLayout}
-          path="/managerTravel/approved"
-        />
-        <RouteWithLayout
-          component={RejectedAndCanceled}
-          exact
-          layout={ManagerLayout}
-          path="/managerTravel/canceled"
-        />
-=======
   return (
     <Switch>
       <Redirect
@@ -172,6 +102,5 @@ const Routes = () => {
     </Switch>
   );
 };
->>>>>>> After rebasing
 
 export default Routes;
