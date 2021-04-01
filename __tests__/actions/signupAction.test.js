7f5b872f935cb1de2f1f180de237513237276472
ALICE-GATERA <alicegatera15@gmail.com>
2021-04-01 08:04:27 +0200

    fix test errors

--- __tests__/actions/signupAction.test.js
+++ __tests__/actions/signupAction.test.js
@@ -1,49 +1,46 @@
-import configureStore from 'redux-mock-store';
-import thunk from 'redux-thunk';
-import * as actions from '../../src/redux/actions/signupRequestAction';
+import configureStore from "redux-mock-store";
+import thunk from "redux-thunk";
+import * as actions from "../../src/redux/actions/signupRequestAction";
 import * as types from "../../src/redux/actions/signupRequestAction";
-import { user } from '../../dummyData'
-import moxios from 'moxios'
-import MockAdapter from 'axios-mock-adapter';
-import axios from 'axios';
-import { request } from 'express';
+import { user } from "../../dummyData";
+import moxios from "moxios";
+import MockAdapter from "axios-mock-adapter";
+import axios from "axios";
+import { request } from "express";
 
-const URL =process.env.REACT_APP_BACKEND_LINK
-const middlewares = [thunk]
+const URL = process.env.REACT_APP_BACKEND_LINK;
+const middlewares = [thunk];
 const mockStore = configureStore(middlewares);
-let store=mockStore({});
-let mock = new MockAdapter(axios)
+let store = mockStore({});
+let mock = new MockAdapter(axios);
 const nextStep = jest.fn();
 
-describe('Fetch Signup actions', () => {
-  
+describe("Fetch Signup actions", () => {
   beforeEach(() => {
-    moxios.install()
-    store = mockStore({signup: {}})
-  })
-  afterEach(() => moxios.uninstall())
+    moxios.install();
+    store = mockStore({ signup: {} });
+  });
+  afterEach(() => moxios.uninstall());
 
-  it('Creates REQUEST_ERROR after task is unsuccessful', () => {
-   
+  it("Creates REQUEST_ERROR after task is unsuccessful", () => {
     moxios.wait(() => {
-      const request = moxios.requests.mostRecent()
+      const request = moxios.requests.mostRecent();
       request.reject({
-       status: 400,
-       response: {
-         data:{
-           error: "\"username\" length must be at least 5 characters long",
-         }
-       }
-          
-       })
-    })
+        status: 400,
+        response: {
+          data: {
+            error: '"username" length must be at least 5 characters long',
+          },
+        },
+      });
+    });
 
     return store.dispatch(actions.requestSignup()).then(() => {
       const expectedActions = store.getActions();
-      expect(expectedActions[0].type).toEqual('REQUEST_SIGNUP')
-      expect(expectedActions[1].type).toEqual('REQUEST_ERROR')
+      expect(expectedActions[0].type).toEqual("REQUEST_SIGNUP");
+      expect(expectedActions[1].type).toEqual("REQUEST_ERROR");
       //expect(store.getActions().type).toEqual(action.type)
-    })
+    });
     // moxios.wait(() => {
     //   const request = moxios.requests.mostRecent()
     //   request.respondWith({
@@ -51,32 +48,31 @@ describe('Fetch Signup actions', () => {
     //    response: {
     //        error: "\"username\" length must be at least 5 characters long",
     //    }
-          
+
     //    })
     // })
 
-  it('Creates REQUEST_SUCCESS after task is successful', () => {
-  
-    moxios.wait(() => {
-      const request = moxios.requests.mostRecent()
-      request.respondWith({
-       status: 200,
-       response: {
-          Message: "User M has been created. Check email for verification"
-       }
-       })
-    })
+    it("Creates REQUEST_SUCCESS after task is successful", () => {
+      moxios.wait(() => {
+        const request = moxios.requests.mostRecent();
+        request.respondWith({
+          status: 200,
+          response: {
+            Message: "User M has been created. Check email for verification",
+          },
+        });
+      });
 
-    return store.dispatch(actions.requestSignup(user, nextStep)).then(() => {
-      const expectedActions = store.getActions();
-      expect(expectedActions[0].type).toEqual('REQUEST_SIGNUP')
-      expect(expectedActions[1].type).toEqual('REQUEST_SUCCESS')
-    })
-  })
-  it('should dispatch the CLOSE_SNACKBAR action', () => {
-    store.clearActions();
-    store.dispatch(actions.closeSnackbar())
-    expect(store.getActions()).toEqual([{"type": "CLOSE_SNACKBAR"}]);
-   })
-
-})
+      return store.dispatch(actions.requestSignup(user, nextStep)).then(() => {
+        const expectedActions = store.getActions();
+        expect(expectedActions[0].type).toEqual("REQUEST_SIGNUP");
+        expect(expectedActions[1].type).toEqual("REQUEST_SUCCESS");
+      });
+    });
+    it("should dispatch the CLOSE_SNACKBAR action", () => {
+      store.clearActions();
+      store.dispatch(actions.closeSnackbar());
+      expect(store.getActions()).toEqual([{ type: "CLOSE_SNACKBAR" }]);
+    });
+  });
+});
