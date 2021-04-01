eb8f9176f95e1de213f8b3e8ef6c02e42c947b83
ALICE-GATERA <alicegatera15@gmail.com>
2021-04-01 06:51:59 +0200

    add pagination

--- src/components/UserSideDrawer/Drawer.js
+++ src/components/UserSideDrawer/Drawer.js
@@ -20,18 +20,10 @@ import {
   ListItemText,
   ListItemIcon,
 } from "@material-ui/core";
-// import {sideBarData} from "./sideBarData";
+import { sideBarData } from "./sideBarData";
 import { Link, useHistory } from "react-router-dom";
 import { getTripLocations } from "../../redux/actions/userTravelHistoryAction";
 import { connect } from "react-redux";
-import DashboardIcon from "@material-ui/icons/Dashboard";
-import AddLocationIcon from "@material-ui/icons/AddLocation";
-import GroupIcon from "@material-ui/icons/Group";
-import CommuteIcon from "@material-ui/icons/Commute";
-import CachedIcon from "@material-ui/icons/Cached";
-import ThumbUpIcon from "@material-ui/icons/ThumbUp";
-import CancelIcon from "@material-ui/icons/Cancel";
-import DoneAllIcon from "@material-ui/icons/DoneAll";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -83,89 +75,6 @@ function DrawerComponent(props) {
   React.useEffect(() => {
     props.getTripLocations();
   }, []);
-
- 
-
-  const sideBarData = [
-    {
-      title: "Dashboard",
-      icon: <DashboardIcon />,
-      link: "/home",
-      subs: [],
-    },
-    {
-      title: "Accomodation",
-      icon: <AddLocationIcon />,
-      link: "/accomodation",
-      subs: [],
-    },
-    {
-      title: "Travel requests",
-      icon: <CommuteIcon />,
-      link: "/travelRequests",
-      subs: [
-        {
-          title: "Create Travel",
-          icon: <CachedIcon />,
-          link: "/travelReqests",
-        },
-        {
-          title: "List of Travel Requests",
-          icon: <ThumbUpIcon />,
-          link: "/travelRequests",
-          subs: [
-            {
-              title: "Pending",
-              icon: <CachedIcon />,
-              link: "/travelRequests",
-            },
-            {
-              title: "Approved",
-              icon: <ThumbUpIcon />,
-              link: "/travelRequests/approved",
-            },
-            {
-              title: "Canceled & Rejected",
-              icon: <CancelIcon />,
-              link: "/travelRequests/canceled",
-            },
-            {
-              title: "Done",
-              icon: <DoneAllIcon />,
-              link: "/travelRequests/done",
-            },
-          ],
-        },
-      ],
-    },
-    {
-      title: "Requester Reports",
-      icon: <CommuteIcon />,
-      link: "/managerTravel",
-      subs: [
-        {
-          title: "Pending",
-          icon: <CachedIcon />,
-          link: "/managerTravel",
-        },
-        {
-          title: "Approved",
-          icon: <ThumbUpIcon />,
-          link: "/managerTravel/approved",
-        },
-        {
-          title: "Canceled & Rejected",
-          icon: <CancelIcon />,
-          link: "/managerTravel/canceled",
-        },
-        {
-          title: "Done",
-          icon: <DoneAllIcon />,
-          link: "/managerTravel/done",
-        },
-      ],
-    }
-  ];
 
   const [drop, setDrop] = React.useState(
     sideBarData.map((link) => {
