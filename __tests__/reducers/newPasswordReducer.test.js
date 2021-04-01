7440bc6226688a1d0bc6c2c51a722663a8faab30
Cyusa144 <69196120+Cyusa144@users.noreply.github.com>
2021-04-01 13:33:08 +0200

    solving failing tests issue

--- __tests__/reducers/newPasswordReducer.test.js
+++ __tests__/reducers/newPasswordReducer.test.js
@@ -1,14 +1,14 @@
 import {NewPasswordReducer} from '../../src/redux/reducers/resetPasswordEmail';
 import {LOADING, SEND_RESET_EMAIL, RESET_PASSWORD, RESET_PASSWORD_SUCCESS, RESET_PASSWORD_FAIL} from '../../src/redux/resetPasswordType';
 const initialState = {
-    isLoading:false,
-    open:false,
+    loading:false,
     success:false,
+    open:false,
     error: '',
     message:''
 }
 const errorState = {
-    isLoading:false,
+    loading:false,
     error: 'User not found',
     open:true,
     message:''
@@ -24,7 +24,7 @@ describe('Reset new password', () =>{
             }
         expect(NewPasswordReducer(initialState, action)).toEqual({
     ...initialState,
-    isLoading:false,
+    loading:false,
     error: 'password must be more than 8 characters',
     open:true,
     message:''
@@ -37,14 +37,14 @@ it('Should handle RESET_PASSWORD without error', () => {
         }
         expect(NewPasswordReducer(initialState, action)).toEqual({
             ...initialState,
-            isLoading:false,
+            loading:false,
             error: '',
             success:true,
-            open:true,
+            open:false,
             message:'password reset successfully 👍 you can login with new password'
         })
     })
     it('should loading before reseting new password', () => {
-        expect(NewPasswordReducer(initialState, {type: LOADING})).toEqual({...initialState,isLoading:true, open:false})
+        expect(NewPasswordReducer(initialState, {type: LOADING})).toEqual({...initialState,loading:true, open:false})
     })
 })
