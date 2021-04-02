81abe0bef3c9eb1ecc44a03152963bce53ecbe1a
CYIMANA Faisal <cyimanafaisal@gmail.com>
2021-04-02 10:49:34 +0200

    after-pull

--- src/routes/Routers.js
+++ src/routes/Routers.js
@@ -9,15 +9,13 @@ import PageNotFound from '../components/views/PageNotFound'
 import Unauthorized from '../components/views/Unauthorized'
 import Profile from '../components/views/Profile';
 import adminHome from '../components/views/Admin/Home';
-import BookForm from '../components/bookAccommodation/form';
-import requesterHome from '../components/views/Requester/Home';
 import CreateRoles from '../components/views/Admin/CreateRoles';
 import SetPermissions from '../components/views/Admin/SetPermissions'
 import ListOfRoles from '../components/views/Admin/ListOfRoles'
 import ListUsers from '../components/views/Admin/ListUsers'
 import { Switch, Redirect } from 'react-router-dom';
 import RouteWithLayout from '../components/RouteWithLayout';
-import {  DefaultLayout, AuthorizedUserLayout, AdminLayout, ErrorLayout, RequesterLayout,ManagerLayout} from '../components/layouts';
+import { DefaultLayout, AuthorizedUserLayout, AdminLayout, ErrorLayout, ManagerLayout } from '../components/layouts';
 import Logout from '../components/views/Logout';
 // import ManagerDashboard from '../components/sideBarDrawer/ManagerDashboard';
 import ManagerTravelDashboard from '../components/manageTravel/manageTravelDashboard'
@@ -85,121 +83,110 @@ const Routes = () => {
         path="/managerTravel/canceled"
       />
 
-        <RouteWithLayout
-          component={Done}
-          exact
-          layout={ManagerLayout}
-          path="/managerTravel/done"
-        />
-        
-         <RouteWithLayout
-          component={signup}
-          exact
-          layout={DefaultLayout}
-          path="/signup"
-        />
-        <RouteWithLayout
-          component={ResetPasswordEmailForm}
-          exact
-          layout={AuthorizedUserLayout}
-          path="/forgetpassword"
-        />
-        <RouteWithLayout
-          component={NewPassword}
-          exact
-          layout={AuthorizedUserLayout}
-          path="/user/reset-password"
-        />
-        <RouteWithLayout
-          component={Logout}
-          exact
-          layout={AuthorizedUserLayout}
-          path="/logout"
-        />
-        <ProtectedRoute
-          component={adminHome}
-          exact
-          layout={AdminLayout}
-          path="/admin"
-        />
-        <ProtectedRoute
-          component={requesterHome}
-          exact
-          layout={RequesterLayout}
-          path="/requester"
-        />
-        <ProtectedRoute
-          component={BookForm}
-          exact
-          layout={RequesterLayout}
-          path="/bookAccommodation/:locationId"
-        />
-        <RouteWithLayout
-          component = {CreateRoles}
-          exact
-          layout={AdminLayout}
-          path="/admin/roles"
-        />
-        <RouteWithLayout
-          component={SetPermissions}
-          exact
-          layout={AdminLayout}
-          path="/admin/permissions"
-        />
-        <RouteWithLayout
-          component={ListOfRoles}
-          exact
-          layout={AdminLayout}
-          path="/admin/roleslist"
-        />
-        <RouteWithLayout
-          component={ListUsers}
-          exact
-          layout={AdminLayout}
-          path="/admin/users"
-        />
-        <RouteWithLayout
-          component={Logout}
-          exact
-          layout={AuthorizedUserLayout}
-          path="/logout"
-        />
-        <RouteWithLayout
-          component={PageNotFound}
-          exact
-          layout={ErrorLayout}
-          path="/PageNotFound"
-        />
-        <RouteWithLayout
-          component={Unauthorized}
-          exact
-          layout={ErrorLayout}
-          path="/unauthorized"
-        />
-        <RouteWithLayout 
-            path="/signup" 
-            component={signup}
-            layout={DefaultLayout}
-        />
-        <RouteWithLayout 
-            path="/user/verification/:token" 
-            component={ verifyAccount }
-            layout={DefaultLayout}
-        />
-        <RouteWithLayout 
-            path="/user/verification/" 
-            component={ verifyAccount }
-            layout={DefaultLayout}
-        />     
-        <ProtectedRoute
-          exact
-          path="/requester/create-travel-request"
-          component={CreateTravelRequest}
-          layout={AuthorizedUserLayout}
-        />
+      <RouteWithLayout
+        component={Done}
+        exact
+        layout={ManagerLayout}
+        path="/managerTravel/done"
+      />
 
-        <Redirect to="/PageNotFound" />
-      </Switch>
+      <RouteWithLayout
+        component={signup}
+        exact
+        layout={DefaultLayout}
+        path="/signup"
+      />
+      <RouteWithLayout
+        component={ResetPasswordEmailForm}
+        exact
+        layout={AuthorizedUserLayout}
+        path="/forgetpassword"
+      />
+      <RouteWithLayout
+        component={NewPassword}
+        exact
+        layout={AuthorizedUserLayout}
+        path="/user/reset-password"
+      />
+      <RouteWithLayout
+        component={Logout}
+        exact
+        layout={AuthorizedUserLayout}
+        path="/logout"
+      />
+      <RouteWithLayout
+        component={adminHome}
+        exact
+        layout={AdminLayout}
+        path="/admin"
+      />
+      <RouteWithLayout
+        component={CreateRoles}
+        exact
+        layout={AdminLayout}
+        path="/admin/roles"
+      />
+      <RouteWithLayout
+        component={SetPermissions}
+        exact
+        layout={AdminLayout}
+        path="/admin/permissions"
+      />
+      <RouteWithLayout
+        component={ListOfRoles}
+        exact
+        layout={AdminLayout}
+        path="/admin/roleslist"
+      />
+      <RouteWithLayout
+        component={ListUsers}
+        exact
+        layout={AdminLayout}
+        path="/admin/users"
+      />
+      <RouteWithLayout
+        component={Logout}
+        exact
+        layout={AuthorizedUserLayout}
+        path="/logout"
+      />
+      <RouteWithLayout
+        component={PageNotFound}
+        exact
+        layout={ErrorLayout}
+        path="/PageNotFound"
+      />
+      <RouteWithLayout
+        component={Unauthorized}
+        exact
+        layout={ErrorLayout}
+        path="/unauthorized"
+      />
+      <RouteWithLayout
+        path="/signup"
+        component={signup}
+        layout={DefaultLayout}
+      />
+      <RouteWithLayout
+        path="/user/verification/:token"
+        component={verifyAccount}
+        layout={DefaultLayout}
+      />
+      <RouteWithLayout
+        path="/user/verification/"
+        component={verifyAccount}
+        layout={DefaultLayout}
+      />
+
+      <ProtectedRoute
+        exact
+        path="/requester/create-travel-request"
+        component={CreateTravelRequest}
+        layout={AuthorizedUserLayout}
+      />
+
+      <Redirect to="/PageNotFound" />
+    </Switch>
   );
 };
 
